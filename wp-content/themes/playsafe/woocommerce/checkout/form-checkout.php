57c328ac0535c7ff38172488dfcafe25af1b2f7c
Scott Brown <accounts@thinkthirdrail.co.uk>
2018-09-29 15:17:51 +0100

    Cart Page

--- wp-content/themes/playsafe/woocommerce/checkout/form-checkout.php
+++ wp-content/themes/playsafe/woocommerce/checkout/form-checkout.php
@@ -39,7 +39,7 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 		<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
 
 		<div class="col2-set" id="customer_details">
-			<div class="col-6">
+			<div class="col-6 ">
 				<?php do_action( 'woocommerce_checkout_billing' ); ?>
 			</div>
 
